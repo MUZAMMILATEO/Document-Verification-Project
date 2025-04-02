2821c4b2c6edc1a52161f789b98d3a089c3c98ed
Muzammil Khan <97946144+MUZAMMILATEO@users.noreply.github.com>
2025-04-02 20:46:05 +0200

    Update template.py

--- template.py
+++ template.py
@@ -24,3 +24,14 @@ lisy_of_filles = [
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
 ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+    else:
+        print(f"file is already present at: {filepath}")
