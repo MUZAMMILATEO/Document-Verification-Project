9e777954863258ff9ab4a679ddc9165d3268bea0
Muzammil Khan <97946144+MUZAMMILATEO@users.noreply.github.com>
2025-04-03 19:46:44 +0200

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Document-Verification-Project",
+    version="0.0.0",
+    author="khanm",
+    author_email="an.ar54040@gmail.com",
+    packages=find_packages()
+)
