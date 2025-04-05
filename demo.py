9c4fa5419025a685e188cfcae9a7e294725059b9
khanm2204 <khanm2204@Ubuntu22.04.myguest.virtualbox.org>
2025-04-05 20:14:16 +0200

    exception handling integrated

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 from DVP.logger import logging
+from DVP.exception import USvisaException
 
 logging.info("Welcome to custom log!")
+
+
+try:
+    a = 2/0
+except Exception as e:
+    raise USvisaException(e, sys)
