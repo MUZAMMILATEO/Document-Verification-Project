a11aba3eeacb2eb9b38f30dec4d608995473da1a
khanm2204 <khanm2204@Ubuntu22.04.myguest.virtualbox.org>
2025-04-14 21:29:47 +0200

    packages updated

--- DVP/components/data_ingestion.py
+++ DVP/components/data_ingestion.py
@@ -4,11 +4,11 @@ import sys
 from pandas import DataFrame
 from sklearn.model_selection import train_test_split
 
-from us_visa.entity.config_entity import DataIngestionConfig
-from us_visa.entity.artifact_entity import DataIngestionArtifact
-from us_visa.exception import USvisaException
-from us_visa.logger import logging
-from us_visa.data_access.usvisa_data import USvisaData
+from DVP.entity.config_entity import DataIngestionConfig
+from DVP.entity.artifact_entity import DataIngestionArtifact
+from DVP.exception import USvisaException
+from DVP.logger import logging
+from DVP.data_access.usvisa_data import USvisaData
 
 
 
