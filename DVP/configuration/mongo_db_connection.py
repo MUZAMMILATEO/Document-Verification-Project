de1bf13c8a1a8fdf4dfa48b59f9e5c685bc3f395
khanm2204 <khanm2204@Ubuntu22.04.myguest.virtualbox.org>
2025-04-14 20:39:09 +0200

    packages updated

--- DVP/configuration/mongo_db_connection.py
+++ DVP/configuration/mongo_db_connection.py
@@ -1,10 +1,10 @@
 import sys
 
-from us_visa.exception import USvisaException
-from us_visa.logger import logging
+from DVP.exception import USvisaException
+from DVP.logger import logging
 
 import os
-from us_visa.constants import DATABASE_NAME, MONGODB_URL_KEY
+from DVP.constants import DATABASE_NAME, MONGODB_URL_KEY
 import pymongo
 import certifi
 
