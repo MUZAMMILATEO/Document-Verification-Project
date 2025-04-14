274bb5f73a38c65134cc15fe12b478888189168a
khanm2204 <khanm2204@Ubuntu22.04.myguest.virtualbox.org>
2025-04-14 20:40:03 +0200

    packages updated

--- DVP/constants/__init__.py
+++ DVP/constants/__init__.py
@@ -5,7 +5,7 @@ DATABASE_NAME = "US_VISA_DATASET.db"
 
 COLLECTION_NAME = "visa_data"
 
-MONGIDB_URL_KEY = "mongodb+srv://khanm:khanm@cluster0.zizpbkz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGODB_URL_KEY = "mongodb+srv://khanm:khanm@cluster0.zizpbkz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 PIPELINE_NAME:str = "usvisa"
 
