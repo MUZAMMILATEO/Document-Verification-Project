848ed4e393fd47dac07a9ce856b86e492730497c
Muzammil Khan <97946144+MUZAMMILATEO@users.noreply.github.com>
2025-04-19 20:41:31 +0200

    Update training_pipeline.py

--- DVP/pipeline/training_pipeline.py
+++ DVP/pipeline/training_pipeline.py
@@ -9,15 +9,15 @@ from DVP.components.data_validation import DataValidation
 # from DVP.components.model_pusher import ModelPusher
 
 
-from DVP.entity.config_entity import (DataIngestionConfig,)
-#                                          DataValidationConfig,
+from DVP.entity.config_entity import (DataIngestionConfig,
+                                          DataValidationConfig,)
 #                                          DataTransformationConfig,
 #                                          ModelTrainerConfig,
 #                                          ModelEvaluationConfig,
 #                                          ModelPusherConfig)
 # 
-from DVP.entity.artifact_entity import (DataIngestionArtifact,)
-#                                             DataValidationArtifact,
+from DVP.entity.artifact_entity import (DataIngestionArtifact,
+                                             DataValidationArtifact,)
 #                                             DataTransformationArtifact,
 #                                             ModelTrainerArtifact,
 #                                             ModelEvaluationArtifact,
@@ -27,7 +27,7 @@ from DVP.entity.artifact_entity import (DataIngestionArtifact,)
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-        # self.data_validation_config = DataValidationConfig()
+        self.data_validation_config = DataValidationConfig()
         # self.data_transformation_config = DataTransformationConfig()
         # self.model_trainer_config = ModelTrainerConfig()
         # self.model_evaluation_config = ModelEvaluationConfig()
@@ -51,7 +51,30 @@ class TrainPipeline:
             return data_ingestion_artifact
         except Exception as e:
             raise USvisaException(e, sys) from e
-        
+
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise USvisaException(e, sys) from e
 
 
 
@@ -61,6 +84,7 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
 
         except Exception as e:
             raise USvisaException(e, sys)
