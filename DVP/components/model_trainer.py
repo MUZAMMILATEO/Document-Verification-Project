d5600d9ce823dca05fe2a2747eb5dc2272538b24
khanm2204 <khanm2204@Ubuntu22.04.myguest.virtualbox.org>
2025-04-20 17:59:01 +0200

    model trainer completed

--- DVP/components/model_trainer.py
+++ DVP/components/model_trainer.py
@@ -8,12 +8,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from neuro_mf  import ModelFactory
 
-from us_visa.exception import USvisaException
-from us_visa.logger import logging
-from us_visa.utils.main_utils import load_numpy_array_data, read_yaml_file, load_object, save_object
-from us_visa.entity.config_entity import ModelTrainerConfig
-from us_visa.entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact, ClassificationMetricArtifact
-from us_visa.entity.estimator import USvisaModel
+from DVP.exception import USvisaException
+from DVP.logger import logging
+from DVP.utils.main_utils import load_numpy_array_data, read_yaml_file, load_object, save_object
+from DVP.entity.config_entity import ModelTrainerConfig
+from DVP.entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact, ClassificationMetricArtifact
+from DVP.entity.estimator import USvisaModel
 
 class ModelTrainer:
     def __init__(self, data_transformation_artifact: DataTransformationArtifact,
