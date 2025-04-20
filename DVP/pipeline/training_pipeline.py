a20e810167aaf12e07f1cb43afbbf41a696a6bc1
Muzammil Khan <97946144+MUZAMMILATEO@users.noreply.github.com>
2025-04-20 21:37:02 +0200

    Update training_pipeline.py

--- DVP/pipeline/training_pipeline.py
+++ DVP/pipeline/training_pipeline.py
@@ -6,22 +6,22 @@ from DVP.components.data_validation import DataValidation
 from DVP.components.data_transformation import DataTransformation
 from DVP.components.model_trainer import ModelTrainer
 from DVP.components.model_evaluation import ModelEvaluation
-# from DVP.components.model_pusher import ModelPusher
+from DVP.components.model_pusher import ModelPusher
 
 
 from DVP.entity.config_entity import (DataIngestionConfig,
                                           DataValidationConfig,
                                           DataTransformationConfig,
                                           ModelTrainerConfig,
-                                          ModelEvaluationConfig,)
-#                                          ModelPusherConfig)
+                                          ModelEvaluationConfig,
+                                          ModelPusherConfig)
 
 from DVP.entity.artifact_entity import (DataIngestionArtifact,
                                              DataValidationArtifact,
                                              DataTransformationArtifact,
                                              ModelTrainerArtifact,
-                                             ModelEvaluationArtifact,)
-#                                             ModelPusherArtifact)
+                                             ModelEvaluationArtifact,
+                                             ModelPusherArtifact)
 
 
 class TrainPipeline:
@@ -31,7 +31,7 @@ class TrainPipeline:
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
         self.model_evaluation_config = ModelEvaluationConfig()
-        # self.model_pusher_config = ModelPusherConfig()
+        self.model_pusher_config = ModelPusherConfig()
 
 
     
@@ -120,6 +120,19 @@ class TrainPipeline:
         except Exception as e:
             raise USvisaException(e, sys)
 
+    def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model pushing
+        """
+        try:
+            model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
+                                       model_pusher_config=self.model_pusher_config
+                                       )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
+
 
 
     def run_pipeline(self, ) -> None:
@@ -132,6 +145,11 @@ class TrainPipeline:
             data_transformation_artifact = self.start_data_transformation(
                   data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+
+            if not model_evaluation_artifact.is_model_accepted:
+                logging.info(f"Model not accepted.")
+                return None
+            model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
 
         except Exception as e:
             raise USvisaException(e, sys)
